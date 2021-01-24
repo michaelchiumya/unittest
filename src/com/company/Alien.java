830733c79ccdb923bcbf20d200e8f9e1ce9fd56a
JackMac <71156794+JackMcIlraith@users.noreply.github.com>
2021-01-24 13:44:04 +0000

    Updated Alien.java to pass and complete all tests Refactored Alien.java to run smoother in line with Martin Fowler's catalogue found at https://refactoring.com/catalog/

--- src/com/company/Alien.java
+++ src/com/company/Alien.java
@@ -23,63 +23,68 @@ public class Alien  {
 
 
     public Alien(boolean dodging, int alienHealth ) {
-        //TODO: Construct the Alien object and initialise the data members appropriately.
-        //J: alienHealth > 0, or set to default value.
-        //dodging is true
-
         this.dodging = dodging;
-        this.alienHealth = alienHealth;
+//input sanitation:
+        if(alienHealth <=0){
+            this.alienHealth = 10; //default health used if no valid input.
+        }
+        else {
+            this.alienHealth = alienHealth;
+        }
         this.setDead(this.alienHealth);
     }
 
 
-    //TODO: Implement appropriate getters and setters for the class
 
     public void dodge() {
-        //TODO: Implement the method
         /*Set dodging to true if the alien is alive*/
         //J: alien will be able to doge once at the start, then subsequent shots will hit, as miss() sets to false.
         //will add rng to compliment after initial code passes as expected.
-        if( alienHealth <= 0)
-            this.dodging = true;
+        if( this.dodging ){
+            this.miss();
+        } else {
+            this.hit();
+        }
 
     }
 
     public void hit() {
-        //TODO: Implement the method
         /*Reduces the alien health by 1. Minimum Alien health should 0.*/
         //j: test to see if alien is dead or not after each hit.
-        if(this.alienHealth >= 1){
-             this.alienHealth--;
-              this.dead = false;
-        }else {
-              this.dead = true;
+        if(this.alienHealth >= 1) {
+            this.alienHealth--;
+            this.setDead(this.alienHealth);
+        } else if (this.alienHealth == 0){
+            System.out.println("Stop, Stop! He's Already Dead");
         }
     }
 
     public void miss() {
-        //TODO: Implement the method
         /*Sets dodging to false*/
         //J: set dodging to false if if the previous shot was dodged.
-        if (dodging == true)
-             dodging = false;
-    }
-
-    public boolean isDead() {
-        return dead;
+        this.dodging = false;
     }
 
     public void setDead(int alienHealth) {
         if(alienHealth <= 0) {
              this.dead = true;
+             this.dodging = false; //dead aliens cannot dodge
+            System.out.println("Alien is now dead");
            }
+        else {
+            this.dead = false;
+        }
     }
 
+
+    //getters for use in testing
     public boolean isDodging() {
         return dodging;
     }
 
-
+    public boolean isDead() {
+        return dead;
+    }
 
     public int getAlienHealth() {
         return alienHealth;
